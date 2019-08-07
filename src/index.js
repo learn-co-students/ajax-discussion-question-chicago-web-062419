5551332e5bf0e62e0590282d0e5c38b40c9d0808
srwatlanta <shawnrwheeler@gmail.com>
2019-08-07 13:35:02 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const fullname = document.getElementById("fullname");
-console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
+const email = document.getElementById("email");
+const street = document.getElementById("street");
+const city = document.getElementById("city")
+const state = document.getElementById('state') //what will fullname evaluate to?
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+  function fetchData(){
+    fetch('https://randomuser.me/api/')
+      .then( res => res.json() )
+      .then( person => renderData(person) )
+}
 });
+
